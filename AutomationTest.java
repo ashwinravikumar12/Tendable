c727ce5c029dfdb7d695df2f73a230f3df94b216
ashwinravikumar12 <150086607+ashwinravikumar12@users.noreply.github.com>
2023-11-07 03:43:15 +0530

    Create AutomationTest.java

---
+++ AutomationTest.java
@@ -0,0 +1,134 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class WebAutomation {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+	
+	WebDriver driver = new ChromeDriver();
+	driver.get("http://www.tendable.com");
+	
+	
+	 confirmAccessibility(driver, "//img[@alt='Tendable Logo']");
+	 confirmAccessibility(driver, "//a[contains(text(),'Our Story')]");
+	 confirmAccessibility(driver, "//a[contains(text(),'Our Solution')]");
+	 confirmAccessibility(driver, "//a[contains(text(),'Why Tendable?')]");
+	 
+	 verifyRequestDemoButton(driver, "//img[@alt='Tendable Logo']");
+	 verifyRequestDemoButton(driver, "//a[contains(text(),'Our Story')]");
+	 verifyRequestDemoButton(driver, "//a[contains(text(),'Our Solution')]");
+	 verifyRequestDemoButton(driver, "//a[contains(text(),'Why Tendable?')]");
+	 
+	 verifyContactUs(driver, "//a[contains(text(),'Contact Us')]");
+	 
+	}
+	
+	
+	public static void confirmAccessibility(WebDriver driver, String xpath) throws InterruptedException {
+	   
+	        WebElement menuButton = driver.findElement(By.xpath(xpath));
+	    	driver.manage().window().maximize();
+	    	Thread.sleep(2000);
+	    	String pName = menuButton.getText();
+	    	 String[] mName = menuButton.getText().split(" ");
+	            String menuName = mName[0];
+	        if (menuButton.isDisplayed()) {
+	           
+	            menuButton.click();	         
+	            String currentURL = driver.getCurrentUrl();
+               
+	            if (currentURL.contains(menuName.toLowerCase())) {
+	                System.out.println("The " +pName  +"menu takes you to the correct page.");
+	            } else {
+	              
+	                System.out.println("The "  +pName  + "menu did not take you to the correct page.");
+	            }
+	        }
+	         else {
+	            System.out.println("The " +pName  + "menu button is not present on the page.");
+	        }	        	        
+}
+
+    public static void verifyRequestDemoButton(WebDriver driver, String menuPath) {
+	       
+            driver.findElement(By.xpath(menuPath)).click();
+    	WebElement reqDemoButton = driver.findElement(By.xpath("//header/div[1]/div[1]/a[3]"));
+    	String pName =driver.findElement(By.xpath(menuPath)).getText();
+
+    	if(reqDemoButton.isDisplayed()|| reqDemoButton.isEnabled()) {
+    		
+    		System.out.println("Request demo is present in " +pName +" menu and Request demo is active");
+    		}
+    	   	
+    	else {
+    		System.out.println("Request demo is NOT available in " +pName +" menu");
+    	}
+    	}
+    	
+    	
+   public static void verifyContactUs(WebDriver driver, String homeContact) throws InterruptedException {
+	   
+	   driver.findElement(By.xpath(homeContact)).click();
+	   String marketContact =   "(//button[@class='body-button bg-plain-600 toggle-control'][normalize-space()='Contact'])[1]";
+		String fullName ="(//input[@id='form-input-fullName'])[1]";
+		String organization ="(//input[@id='form-input-organisationName'])[1]";
+		String phone ="(//input[@id='form-input-cellPhone'])[1]";
+		String email ="(//input[@id='form-input-email'])[1]";
+		String jobRole = "(//select[@id='form-input-jobRole'])[1]";
+		String radioButton="//div[@class='fixed right-0 top-1/4 z-50 px-3.5 xl:px-7 py-3 bg-transparent flex flex-col space-y-3']";
+		String submit ="(//button[@data-loading-text='Loading...'][normalize-space()='Submit'])[1]";
+		String errorMsg="/html[1]/body[1]/div[5]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]";
+		
+	   driver.findElement(By.xpath(marketContact)).click();
+	   driver.findElement(By.xpath(fullName)).sendKeys("Rahul");
+	   driver.findElement(By.xpath(organization)).sendKeys("MNC");
+	   driver.findElement(By.xpath(phone)).sendKeys("903998398938");
+	   driver.findElement(By.xpath(email)).sendKeys("rahul124@gmail.com");
+
+	  WebElement dropdown= driver.findElement(By.xpath(jobRole));
+	  
+	  Select select = new Select(dropdown);
+	  select.selectByVisibleText("Management");
+	  JavascriptExecutor js = (JavascriptExecutor) driver;
+
+      js.executeScript("window.scrollBy(0, 500);");
+   
+      WebDriverWait wait1 = new WebDriverWait(driver,Duration.ofSeconds(10));
+      WebElement radButton = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath(radioButton)));
+      
+      Actions actions = new Actions(driver);
+      
+      actions.doubleClick(radButton).perform();
+      
+      WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+      WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(submit)));
+
+      submitButton.click();	 
+       
+	  String ActualError = driver.findElement(By.xpath(errorMsg)).getText();
+	  String ExpectedError = "Sorry, there was an error submitting the form. Please try again.";
+
+	 if(ExpectedError.contains(ActualError)){
+		 System.out.println("error message displayed successfully. Test Passed");
+	 }
+	 else {
+		 System.out.println("error message displayed successfully. Test Failed");
+
+	 }
+	 
+	 }
+	 
+}
